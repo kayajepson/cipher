79e28362ee27ed4692721896e2a97f7bcb3e16b8
Alex Martinez and Kaya Jepson <alex@>
2019-03-25 15:51:31 -0700

    add function that takes first and last letter and capitalizes them

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,10 @@ var sentence = prompt("Enter a sentence:");
 console.log(sentence);
 
 var capital = function (placeholder) {
-  console.log(placeholder.toUpperCase());
+  console.log(placeholder.charAt(0).toUpperCase());
+  console.log(placeholder.slice(-1).toUpperCase());
 };
 
-var firstChar = function(placeholder2) {
-  console.log(placeholder2.charAt(0));
-};
-
-var lastChar = function(placeholder3) {
-  console.log((placeholder3).slice(-1));
-}
 capital(sentence);
 firstChar(sentence);
 lastChar(sentence);
