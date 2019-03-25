67f493c3ce6727545a848cb88a65e8e0d62a9044
Alex Martinez and Kaya Jepson <alex@>
2019-03-25 16:03:43 -0700

    add idk

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 var sentence = prompt("Enter a sentence:");
 console.log(sentence);
 
-var capital = function (placeholder) {
-  console.log(placeholder.charAt(0).toUpperCase());
-  console.log(placeholder.slice(-1).toUpperCase());
+var capital1 = function (placeholder1) {
+  console.log(placeholder1.charAt(0).toUpperCase());
+  //console.log(placeholder.slice(-1).toUpperCase());
 };
 
-capital(sentence);
-firstChar(sentence);
-lastChar(sentence);
+var capital2 = function (placeholder2) {
+  //console.log(placeholder.charAt(0).toUpperCase());
+  console.log(placeholder2.slice(-1).toUpperCase());
+};
+
+capital1(sentence);
+capital2(sentence);
+
+//var attach = function (placeholder1 , placeholder2) {
+//  console.log(placeholder1 + placeholder2);
+//};
+
+//attach(capital1, capital2);
+
+//firstChar(sentence);
+//lastChar(sentence);
