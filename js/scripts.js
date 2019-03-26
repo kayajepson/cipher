ebf0d063f1b0487444990f653127088253c35219
Kaya Jepson <jepsonkaya@gmail.com>
2019-03-26 02:01:24 +0000

    add reversal

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,17 @@ var sentence = prompt("Enter a sentence:");
 console.log(sentence);
 
 var capital1 = function (string) {
-  console.log(string.charAt(0).toUpperCase() + string.slice(-1).toUpperCase());
+  return string.charAt(0).toUpperCase() + string.slice(-1).toUpperCase();
 };
 
+var out = capital1(sentence);
 
-capital1(sentence);
+
+var reverse = function (string) {
+  return string.slice(-1) + string.charAt(0);
+};
+
+console.log(reverse(out));
 //var two = capital2(sentence);
 //console.log(one.concat(two));
 //var attach = function (placeholder1 , placeholder2) {
@@ -17,3 +23,15 @@ capital1(sentence);
 
 //firstChar(sentence);
 //lastChar(sentence);
+
+// // var catName = prompt("what is your cat's name? ");
+// // //
+// // // var cat = function(placeholder) {
+// // //   alert(placeholder);
+// // // };
+// // //
+// // // cat(catName);
+// //
+// // function alertName(catName) {alert(catName)};
+//
+// alertName(catName);
