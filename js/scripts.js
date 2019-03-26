d832b66b22ea1a3dcd26a72d20cc219ae09bb532
Kaya Jepson <jepsonkaya@gmail.com>
2019-03-26 02:24:29 +0000

    clean up functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 var sentence = prompt("Enter a sentence:");
 console.log(sentence);
 
-var capital1 = function (string) {
+// var capitalize = function (string) {
+//   return string.charAt(0).toUpperCase() + string.slice(-1).toUpperCase();
+// };
+
+function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(-1).toUpperCase();
 };
 
-var out = capital1(sentence);
-
-
-var reverse = function (string) {
+function reverse(string) {
   return string.slice(-1) + string.charAt(0);
 };
 
-console.log(reverse(out));
+function final(sentence2) {
+  var out = capitalize(sentence2);
+  return reverse(out);
+}
+
+console.log(final("what"));
+
 //var two = capital2(sentence);
 //console.log(one.concat(two));
 //var attach = function (placeholder1 , placeholder2) {
